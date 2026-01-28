38176618102d5063fdfa7013a5dca4a5a1a4f103
JayapriyaSiakumar <jayapriyaeswari05@gmail.com>
2026-01-28 14:06:12 +0530

    profile pic container updated

--- src/Pages/common/EventPayments.jsx
+++ src/Pages/common/EventPayments.jsx
@@ -58,7 +58,7 @@ const EventPayments = ({ eventId }) => {
                     <div className="flex items-center">
                       <div className="flex shrink-0 h-10 w-10">
                         <img
-                          className="h-10 w-10 rounded-full"
+                          className="h-10 w-10 rounded-full object-cover"
                           src={
                             payment.profileImage
                               ? payment.profileImage
